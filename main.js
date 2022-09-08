a3b73bb55023accd4a3dbd3df01a69b636344199
ssawrav <s.sawrav@gmail.com>
2022-09-08 12:48:58 -0400

    JS:Wire the function to inputs & submit button
    - Make sure the input values are of NUmber data type only
    by using Number()
    - Create a displayOutput function to take care of displayong output
    - This completes the base functional program
    
    Signed-off-by: ssawrav <s.sawrav@gmail.com>

--- main.js
+++ main.js
@@ -4,18 +4,29 @@ var currentPrice = document.querySelector('#current-price');
 var submitBtn = document.querySelector('#submit-btn');
 var outputBox = document.querySelector('#output-box');
 
+submitBtn.addEventListener('click', submitHandler);
+
+function submitHandler() {
+    var initialP = Number(initialPrice.value);
+    var quantitySt = Number(quantityStocks.value);
+    var currPrice = Number(currentPrice.value);
+    calculateProfitAndLoss(initialP, quantitySt, currPrice);
+}
+
 function calculateProfitAndLoss (initial, quantity, current) {
     if (initial > current) {
         var profit = (initial - current) * quantity;
         var profitPercentage = (profit/current) * 100;
-        console.log('Yay! you made a profit of ' + profit + ' in your investment with a profit percentage of ' + profitPercentage + '%');
+        displayOutput('Yay! you made a profit of ' + profit + ' in your investment with a profit percentage of ' + profitPercentage + '%');
     } else if (current > initial) {
         var loss = (current - initial) * quantity;
         var lossPercentage = (loss/current) * 100;
-        console.log('Oh no! you made a loss of ' + loss + ' in your investment with a loss percentage of ' + lossPercentage + '%');  
+        displayOutput('Oh no! you made a loss of ' + loss + ' in your investment with a loss percentage of ' + lossPercentage + '%');  
     } else {
-        console.log('Seems like you still have what you invested.')
+        displayOutput('Seems like you still have what you invested.');
     }
 }
 
-calculateProfitAndLoss(100, 10, 1000);
+function displayOutput(message) {
+    outputBox.innerHTML = message;
+}
